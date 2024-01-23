c8ecf3dc17ca0a6195994d22adb3e60be2193191
h676671 <676671@stud.hvl.no>
2024-01-23 11:21:12 +0100

    Oppgave 1.1 ferdig

--- src/main/java/no/hvl/data102/filmarkiv/impl/Film.java
+++ src/main/java/no/hvl/data102/filmarkiv/impl/Film.java
@@ -1,5 +1,7 @@
 package main.java.no.hvl.data102.filmarkiv.impl;
 
+import java.util.Objects;
+
 public class Film {
     private int filmNr;
     private String filmSkaper;
@@ -23,4 +25,58 @@ public class Film {
 
     }
 
+    public void setFilmNr(int filmNr) {
+        this.filmNr = filmNr;
+    }
+    public int getFilmNr() {
+        return filmNr;
+    }
+
+    public void setFilmSkaper(String filmSkaper) {
+        this.filmSkaper = filmSkaper;
+    }
+    public String getFilmSkaper() {
+        return this.filmSkaper;
+    }
+
+    public void setTittel(String tittel) {
+        this.tittel = tittel;
+    }
+    public String getTittel() {
+        return this.tittel;
+    }
+
+    public void setUtgivelse(int utgivelse){
+        this.utgivelse = utgivelse;
+    }
+    public int getUtgivelse() {
+        return this.utgivelse;
+    }
+
+    public void setSjanger(Enum sjanger) {
+        this.sjanger = sjanger;
+    }
+    public Enum getSjanger() {
+        return this.sjanger;
+    }
+
+    public void setFilmSelskap(String filmSelskap) {
+        this.filmSelskap = filmSelskap;
+    }
+    public String getFilmSelskap() {
+        return this.filmSelskap;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Film film = (Film) o;
+        return filmNr == film.filmNr && utgivelse == film.utgivelse && Objects.equals(filmSkaper, film.filmSkaper) && Objects.equals(tittel, film.tittel) && Objects.equals(sjanger, film.sjanger) && Objects.equals(filmSelskap, film.filmSelskap);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(filmNr, filmSkaper, tittel, utgivelse, sjanger, filmSelskap);
+    }
 }

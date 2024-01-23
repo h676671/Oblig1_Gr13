2e22129d986021eb0831d994990f44d7a218362d
h676671 <676671@stud.hvl.no>
2024-01-23 10:51:48 +0100

    Lagt til Film klassen og konstruktører

--- src/main/java/no/hvl/data102/filmarkiv/impl/Film.java
+++ src/main/java/no/hvl/data102/filmarkiv/impl/Film.java
@@ -1,4 +1,26 @@
 package no.hvl.data102.filmarkiv.impl;
 
 public class Film {
+    private int filmNr;
+    private String filmSkaper;
+    private String tittel;
+    private int utgivelse;
+    private Enum sjanger;
+    private String filmSelskap;
+
+    public Film() {
+        this(0,"","",0,"","");
+    }
+
+    public Film(int filmNr, String filmSkaper, String tittel, int utgivelse, Enum sjanger, String filmSelskap) {
+
+        this.filmNr = filmNr;
+        this.filmSkaper = filmSkaper;
+        this.tittel = tittel;
+        this.utgivelse = utgivelse;
+        this.sjanger = sjanger;
+        this.filmSelskap = filmSelskap;
+
+    }
+
 }

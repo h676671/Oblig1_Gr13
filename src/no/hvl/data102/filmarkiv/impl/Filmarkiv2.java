8f7e95c6918eca321b20d14875fca480ac528c47
671465 <andreasbenestad4@gmail.com>
2024-01-28 23:02:01 +0100

    Oppagave2

--- src/no/hvl/data102/filmarkiv/impl/Filmarkiv2.java
+++ src/no/hvl/data102/filmarkiv/impl/Filmarkiv2.java
@@ -3,5 +3,45 @@ public class Filmarkiv2 implements FilmarkivADT {
 	
 	private int antall;
 	private LinearNode<Film> start;
+	
+	public Filmarkiv2() {
+		antall = 0;
+		start= null;
+	}
+	
+	@Override
+	public Film finnFilm(int nr) {
+		return null;
+	}
 
+	@Override
+	public void leggTilFilm(Film nyFilm) {
 }
+	
+	@Override
+	public boolean slettFilm(int filmnr) {
+		return false;
+	}
+	public Film[] søkTittel(String delstreng) {
+		return null;
+	}
+	
+	@Override
+	public Film[] sølProdusent(String delstreng) {
+		return null;
+	}
+	
+	@Override
+	public int antall(Sjanger sjanger) {
+		return 0;
+	}
+	@Override
+	public int antall() {
+		return antall;
+	}
+	
+	private LinearNode<Film> finnNode(int filmnr){
+		return null;
+	}
+	
+}	
